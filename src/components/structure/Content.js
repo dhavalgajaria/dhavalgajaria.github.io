5aa3624108f95ca347e4f00ba1527a77fd9bc5ed
krishamehta <kkoolkrisha@gmail.com>
2020-02-04 18:57:56 -0700

    Fist commit yay

---
+++ src/components/structure/Content.js
@@ -0,0 +1,17 @@
+import React from "react"
+import AboutMe from "../sections/AboutMe"
+import Skills from "../sections/Skills"
+import Experience from "../sections/Experience"
+import Articles from "../sections/CommentedArticles"
+
+function Content() {
+    return(
+        <main>
+            <AboutMe />
+            <Skills />
+            <Experience />
+            </main>
+    )
+}
+
+export default Content
