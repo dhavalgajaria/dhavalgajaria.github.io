994772e7325dc25ad2104e9c3434488c63db6d7f
krishamehta <kkoolkrisha@gmail.com>
2020-02-04 19:13:22 -0700

    Updates

---
+++ precache-manifest.dae649db1c3f0b807a7c45b00132e48a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04eb97e61d9868077c75",
+    "url": "/static/js/main.d4c0519b.chunk.js"
+  },
+  {
+    "revision": "c9f7bc571661205fe9d1",
+    "url": "/static/js/2.ce8a9bb8.chunk.js"
+  },
+  {
+    "revision": "76de3840a758e88bc3756ebdf49ec0f7",
+    "url": "/index.html"
+  }
+];
